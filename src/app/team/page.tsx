351769c518f720302a67d79f529d54717347f033
JoyBoyIsHere-D <jayanshujubujena@gmail.com>
2024-12-30 13:05:21 +0530

    Initial Team Page Commit

---
+++ src/app/team/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    return (
+      <div className="h-[800px] flex flex-col items-center justify-center">
+        <h1>Team</h1>
+        <p>Welcome to the Teams Page</p>
+      </div>
+    );
+  }
+  
