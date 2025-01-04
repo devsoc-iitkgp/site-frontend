ead2cabfd32c232c5ea82480d63f503667d577f7
Akshat-jwr <akshat2306jwr3@gmail.com>
2025-01-04 17:26:55 +0530

    Responsiveness in hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <div className="flex h-[800px] flex-col items-center justify-center">
-      <h1>Home</h1>
-      <p>Welcome to the Home page!</p>
+      <HeroSection />
     </div>
   );
 }
