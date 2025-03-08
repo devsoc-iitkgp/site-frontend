3c15683342854154ef36bf96f1483f836b0edfc5
Devansh Soni <devanshsoni899@gmail.com>
2025-03-08 22:01:22 +0530

    some minor positioning changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import About from "@/components/about";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      <About/>
+      <About />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,24 +12,28 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-       backgroundImage: {
-         'glass-feature-card': "url('/Glass_feauture_card_bg.svg')",
-         'number-bullet-bg': "url('/number_bullet-bg.svg')",
-         'bullet-path': "url('/bullet-path.png')",
-         'apnaInsti-path-logo': "url('/apnaInsti-path-logo.svg')",
-         'apnaInsti-ss-bg': "url('/apnaInsti_ss_bg.svg')",
+      backgroundImage: {
+        "glass-feature-card": "url('/Glass_feauture_card_bg.svg')",
+        "number-bullet-bg": "url('/number_bullet-bg.svg')",
+        "bullet-path": "url('/bullet-path.png')",
+        "apnaInsti-path-logo": "url('/apnaInsti-path-logo.svg')",
+        "apnaInsti-ss-bg": "url('/apnaInsti_ss_bg.svg')",
+        "phone-bg": "url('/phone_bg.svg')",
       },
-        fontFamily: {
-          poppins: ['Poppins', 'sans-serif'], // Add Poppins font
-          montserrat: ['Montserrat', 'sans-serif'],
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Add Poppins font
+        montserrat: ["Montserrat", "sans-serif"],
       },
-        rotate: {
-          '20': '20deg',
-          '25': '25deg',
-          '22.5': '22.5deg',
-          '15': '15deg',
-          '345': '345deg',
-      }
+      rotate: {
+        "20": "20deg",
+        "25": "25deg",
+        "22.5": "22.5deg",
+        "15": "15deg",
+        "345": "345deg",
+      },
+      screens: {
+        hd: "1080px", // Custom screen for extra small devices
+      },
     },
   },
   plugins: [],
